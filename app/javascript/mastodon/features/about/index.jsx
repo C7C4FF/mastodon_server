5d1e0ce254ac4bf356174bc11c06f3689621f36e
C7C4FF <c7c4ff.beom@gmail.com>
2026-06-20 01:53:11 +0900

    style: 비로그인 about 화면 수정

--- app/javascript/mastodon/features/about/index.jsx
+++ app/javascript/mastodon/features/about/index.jsx
@@ -18,6 +18,7 @@ import { NavigationFocusTarget } from 'mastodon/components/navigation_focus_targ
 import { ServerHeroImage } from 'mastodon/components/server_hero_image';
 import { Skeleton } from 'mastodon/components/skeleton';
 import { LinkFooter} from 'mastodon/features/ui/components/link_footer';
+import { identityContextPropShape, withIdentity } from 'mastodon/identity_context';
 
 import { Section } from './components/section';
 import { RulesSection } from './components/rules';
@@ -54,6 +55,7 @@ const mapStateToProps = state => ({
 class About extends PureComponent {
 
   static propTypes = {
+    identity: identityContextPropShape,
     server: ImmutablePropTypes.map,
     locale: ImmutablePropTypes.string,
     extendedDescription: ImmutablePropTypes.map,
@@ -80,7 +82,28 @@ class About extends PureComponent {
 
   render () {
     const { multiColumn, intl, server, extendedDescription, domainBlocks, locale } = this.props;
+    const { signedIn } = this.props.identity;
     const isLoading = server.isLoading;
+
+    if (!signedIn) {
+      return (
+        <Column bindToDocument={!multiColumn} label={intl.formatMessage(messages.title)}>
+          <div className='scrollable about about--signed-out' id={getColumnSkipLinkId(1)}>
+            <NavigationFocusTarget as='div' className='about__signed-out-content'>
+              <p className='about__signed-out-message'>서버의 유저가 아니면 접근할 수 없습니다.</p>
+              <a href='/auth/sign_in' className='button about__signed-out-button'>
+                <FormattedMessage id='sign_in_banner.sign_in' defaultMessage='Login' />
+              </a>
+            </NavigationFocusTarget>
+          </div>
+
+          <Helmet>
+            <title>{intl.formatMessage(messages.title)}</title>
+            <meta name='robots' content='noindex' />
+          </Helmet>
+        </Column>
+      );
+    }
 
     return (
       <Column bindToDocument={!multiColumn} label={intl.formatMessage(messages.title)}>
@@ -187,4 +210,4 @@ class About extends PureComponent {
 
 }
 
-export default connect(mapStateToProps)(injectIntl(About));
+export default connect(mapStateToProps)(withIdentity(injectIntl(About)));
